0c595904d6b1eea6e9064739b458d251f7a802a5
MEA <mayer_evgenija@mail.ru>
2021-08-17 14:01:29 +0700

    во фрагменте NoteDescriptionFragment создала fragment, в который будем помещать имя заметки
    во фрагменте переопределила метод onCreate, в котором будем получать аргумент (имя заметки). В макете layout_fragment_note_descrip добавила 2 текст вью - для имени заметки и текста заметки.

--- app/src/main/java/keyone/keytwo/notes/NoteDescriptionFragment.java
+++ app/src/main/java/keyone/keytwo/notes/NoteDescriptionFragment.java
@@ -8,9 +8,23 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 public class NoteDescriptionFragment extends Fragment {
+    public static String ARG_NOTE = "note";
+    private Note note;
 
-    public static NoteDescriptionFragment newInstance() {
-        return new NoteDescriptionFragment();
+    public static NoteDescriptionFragment newInstance(Note note) {
+        NoteDescriptionFragment fragment = new NoteDescriptionFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(ARG_NOTE, note);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.note = getArguments().getParcelable(ARG_NOTE);
+        }
     }
 
     @Override
