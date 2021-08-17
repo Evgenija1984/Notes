e8ef714be9ac3d1ec325d220fd4892a54c0582bf
MEA <mayer_evgenija@mail.ru>
2021-08-17 14:51:24 +0700

    В NoteNameFragment в методе onCreateView получаем список заметок из массива в ресурсах

--- app/src/main/java/keyone/keytwo/notes/NoteNameFragment.java
+++ app/src/main/java/keyone/keytwo/notes/NoteNameFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +18,17 @@ public class NoteNameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_fragment_note_name, container, false);
+        LinearLayout linearLayout = (LinearLayout) view;
+        String[] notes = getResources().getStringArray(R.array.noteNameArray);
+        for(int i = 0; i < notes.length; i++) {
+            String name = notes[i];
+            TextView textViewName = new TextView(getContext());
+            textViewName.setText(name);
+            textViewName.setTextSize(30);
+            linearLayout.addView(textViewName);
+
+        }
+
         return view;
     }
 }
