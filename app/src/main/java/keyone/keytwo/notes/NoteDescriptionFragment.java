0f23c1e5b73b39c091f5deee485f6b011b72de3c
MEA <mayer_evgenija@mail.ru>
2021-08-17 12:30:59 +0700

    создала фрагмент NoteDescriptionFragment и макет layout_fragment_note_descrip для текста заметки
    переименовала макет для фрагмента NoteNameFragment

--- app/src/main/java/keyone/keytwo/notes/NoteDescriptionFragment.java
+++ app/src/main/java/keyone/keytwo/notes/NoteDescriptionFragment.java
@@ -7,15 +7,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class NoteNameFragment extends Fragment {
+public class NoteDescriptionFragment extends Fragment {
 
-    public static NoteNameFragment newInstance() {
-        return new NoteNameFragment();
+    public static NoteDescriptionFragment newInstance() {
+        return new NoteDescriptionFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.layout_fragment_note_name, container, false);
+        View view = inflater.inflate(R.layout.layout_fragment_note_descrip, container, false);
         return view;
     }
 }
